b2be4d6432b60ffc6b2e5d4beae79e465d6a7994
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 19:09:33 -0300

    Idioma.cpp

--- Idioma.cpp
+++ Idioma.cpp
@@ -1,27 +1,8 @@
-#include "Idioma.h" // Inclusão do arquivo de cabeçalho Idioma.h
-#include <iostream> 
+    #include "Idioma.h"
+    #include <string>
 
-using namespace std;
+    Idioma::Idioma(std::string nome) : nome(nome) {}
 
-Idioma::Idioma(string nome) : nome(nome) {} // Construtor da classe Idioma
-
-string Idioma::getNome() const { 
-    return nome; // Retorna o nome do idioma
-}
-
-void Idioma::setNome(const string& nome) {
-    this->nome = nome; // Atribui um novo nome ao idioma
-}
-
-bool Idioma::operator<(const Idioma& other) const {
-    return nome < other.nome; 
-}
-
-bool Idioma::operator==(const Idioma& other) const {
-    return nome == other.nome;
-}
-
-bool Idioma::operator!=(const Idioma& other) const { 
-    return nome != other.nome;
-}
-
+   std::string Idioma::getNome() const {
+       return nome;
+    }
