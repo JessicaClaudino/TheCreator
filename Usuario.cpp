8dc9673e7200e5d5f5273ac1c24f04aa923e04c8
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 14:32:44 -0300

    Usuario.cpp

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,22 +1,12 @@
 #include "Usuario.h"
-#include <iostream> // Incluímos iostream para cout e endl
-
-using namespace std;
-
 Usuario::Usuario(string nome) : nome(nome) {}
 
 string Usuario::getNome() const {
     return nome;
 }
-
-void Usuario::setNome(const string& nome) {
-    this->nome = nome;
+void Usuario::setMusicasFavoritas(const std::vector<Musica*>& musicas){
+   this->musicasFavoritas = musicas;
 }
-
-void Usuario::adicionarMusicaFavorita(const Musica& musica) {
-    musicasFavoritas.push_back(musica);
-}
-
-vector<Musica> Usuario::listarMusicasFavoritas() const {
-    return musicasFavoritas;
-}
+ const std::vector<Musica*> &Usuario::getMusicasFavoritas() const{
+   return musicasFavoritas;
+ }
