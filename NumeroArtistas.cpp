7f46b97e77b7e030f60ed9fca8eb2573bfe80a4b
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 19:05:30 -0300

    NumeroArtistas.cpp

--- NumeroArtistas.cpp
+++ NumeroArtistas.cpp
@@ -2,10 +2,12 @@
 #include "Musica.h"
 #include <set>
 
-int NumeroArtistas::calcularNumeroArtistas(const std::vector<Musica*>& musicas) {
-    std::set<std::string> artistasDistintos;
-        for (const Musica* musica : musicas) {
-           artistasDistintos.insert(musica->artista.getNome());
+int NumeroArtistas::calcularNumeroArtistas(const std::vector<Musica*>& musicas) const {
+    std::set<std::string> artistasUnicos;
+    for (const Musica* musica : musicas) {
+        if (musica && musica->getArtista()) { // Verifica se o ponteiro e o artista são válidos
+            artistasUnicos.insert(musica->getArtista()->getNome()); // Adiciona o artista 
         }
-     return artistasDistintos.size();
+    }
+    return artistasUnicos.size();
 }
