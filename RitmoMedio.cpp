6fdbee8d81899b93c1e33d139110352e0fb41ec7
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 18:57:49 -0300

    RitmoMedio.cpp

--- RitmoMedio.cpp
+++ RitmoMedio.cpp
@@ -1,15 +1,19 @@
-#include "RitmoMedio.h"
-#include "Musica.h"
-#include <vector>
-#include <numeric>
+ #include "RitmoMedio.h"
+ #include "Musica.h"
+ #include <numeric>
 
-double RitmoMedio::calcular(const std::vector<Musica*>& musicas) {
-    if (musicas.empty()) {
-        return 0.0;
-    }
-   double soma = 0;
-   for(const Musica* musica : musicas) {
-       soma += musica->getBpm();
-    }
-   return soma / musicas.size();
-}
+ double RitmoMedio::calcularRitmoMedio(const std::vector<Musica*>& musicas) const {
+      if (musicas.empty()) return 0.0;
+
+     double totalRitmo = 0.0;
+     int numMusicas = 0;
+    for (const Musica* musica : musicas) {
+         if (musica != nullptr && musica->getRitmo() != nullptr) {
+          totalRitmo += musica->getRitmo()->getBpm();
+          numMusicas++;
+         }
+
+     }
+
+    return (numMusicas > 0) ? totalRitmo / numMusicas : 0.0;
+ }
