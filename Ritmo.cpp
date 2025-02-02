4e23a7ce25e2d5b7d29be4c8f8d0f5f05e3c1806
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 18:55:59 -0300

    Ritmo.cpp

--- Ritmo.cpp
+++ Ritmo.cpp
@@ -1,22 +1,11 @@
 #include "Ritmo.h"
-#include "Musica.h" // usando musica.h de uma vez
 
 Ritmo::Ritmo(int bpm) : bpm(bpm) {}
 
 int Ritmo::getBpm() const {
-  return bpm;
+    return bpm;
 }
 
 void Ritmo::setBpm(int bpm) {
-  this->bpm = bpm;
-}
-
-int Ritmo::calcularBPMEspecifico(const std::vector<Musica*> &musicas, const std::string &nomeMusica) {
-int soma = 0;
-    for (const Musica* musica : musicas){
-        if (musica->getNome() == nomeMusica){
-            soma += musica->getBpm();
-        }
-    }
-    return soma;
+    this->bpm = bpm;
 }
