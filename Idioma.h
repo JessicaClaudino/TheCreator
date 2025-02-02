8fc87de4df63450e637e28be5643953a897df21f
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 19:08:23 -0300

    Idioma.h

--- Idioma.h
+++ Idioma.h
@@ -1,20 +1,14 @@
-#pragma once // Evita que o código seja incluído mais de uma vez
-#include <string> // Inclui a biblioteca string
-#include "Idioma.h" // Inclui Idioma.h para ter acesso a classe Idioma
+#ifndef IDIOMA_H
+#define IDIOMA_H
+#include <string>
 
-using namespace std;
 
-class Idioma { 
+class Idioma {
+private:
+    std::string nome; // Adicione std::
 public:
-    string nome; // Atributo nome do idioma
-
-    Idioma(string nome); 
-    Idioma() = default; 
-
-    string getNome() const; // Função que retorna o nome do idioma
-    void setNome(const string& nome); // Função que modifica o nome do idioma
-
-    bool operator<(const Idioma& other) const;
-    bool operator==(const Idioma& other) const;
-    bool operator!=(const Idioma& other) const; 
+    Idioma(std::string nome); // Adicione std::
+    std::string getNome() const; // Adicione std::
 };
+
+#endif
