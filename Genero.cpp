3e315f99eedd594a0a911902a3a919140ea8b584
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 12:23:43 -0300

    Update Genero.cpp

--- Genero.cpp
+++ Genero.cpp
@@ -1,25 +1,11 @@
 #include "Genero.h"
 
-using namespace std;
+Genero::Genero(std::string genero) : genero(genero) {}
 
-Genero::Genero(string nome) : nome(nome) {}
-
-string Genero::getNome() const {
-    return nome;
+std::string Genero::getGenero() const {
+    return genero;
 }
 
-void Genero::setNome(const string& nome) {
-    this->nome = nome;
-}
-
-bool Genero::operator<(const Genero& other) const {
-    return nome < other.nome;
-}
-
-bool Genero::operator==(const Genero& other) const {
-    return nome == other.nome;
-}
-
-bool Genero::operator!=(const Genero& other) const { // Implementação do operador !=
-    return nome != other.nome;
+void Genero::setGenero(const std::string& genero) {
+    this->genero = genero;
 }
