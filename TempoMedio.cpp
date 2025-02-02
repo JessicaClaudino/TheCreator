ffc918c0b8df679362f9bf324a9c69dcdd0d7cdc
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 19:00:07 -0300

    TempoMedio.cpp

--- TempoMedio.cpp
+++ TempoMedio.cpp
@@ -1,15 +1,19 @@
-#include "TempoMedio.h"
-#include "Musica.h"
-#include <vector>
-#include <numeric>
+ #include "TempoMedio.h"
+ #include "Musica.h"
+ #include <numeric>
 
-double TempoMedio::calcular(const std::vector<Musica*>& musicas) {
-    if (musicas.empty()) {
-        return 0.0;
-    }
-    double soma = 0;
+ double TempoMedio::calcularTempoMedio(const std::vector<Musica*>& musicas) const {
+     if (musicas.empty()) return 0.0;
+
+     double totalSegundos = 0.0;
+     int numMusicas = 0;
+
      for (const Musica* musica : musicas) {
-        soma += std::stod(musica->getTempo()->toString());
+         if (musica != nullptr && musica->getTempo() != nullptr) {
+            totalSegundos += musica->getTempo()->getMinutos() * 60 + musica->getTempo()->getSegundos();
+              numMusicas++;
+         }
     }
-    return soma / musicas.size();
+
+     return (numMusicas > 0) ? totalSegundos / numMusicas : 0.0;
 }
