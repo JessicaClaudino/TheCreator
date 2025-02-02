aca16916744455db2fdee409e20aea9d64b54992
JessicaClaudino <jessica.claudino.07@gmail.com>
2025-02-02 18:52:08 -0300

    NumeroGeneros.cpp

--- NumeroGeneros.cpp
+++ NumeroGeneros.cpp
@@ -1,14 +1,12 @@
-#include "NumeroGeneros.h"
-#include <set>
+    #include "NumeroGeneros.h"
+    #include <set>
 
-int NumeroGeneros::calcularNumeroGeneros(const std::vector<Musica*>& musicas) const {
-    std::set<std::string> generosUnicos;
-
-    for (const Musica* musica : musicas) {
-        if (musica != nullptr && musica->getGenero() != nullptr) {
-            generosUnicos.insert(musica->getGenero()->getNome());
+    int NumeroGeneros::calcularNumeroGeneros(const std::vector<Musica*>& musicas) const {
+        std::set<std::string> generosUnicos;
+        for (const Musica* musica : musicas) {
+            if (musica != nullptr && musica->getGenero() != nullptr) {
+                generosUnicos.insert(musica->getGenero()->getNome());
+            }
         }
+        return generosUnicos.size();
     }
-
-    return generosUnicos.size();
-}
